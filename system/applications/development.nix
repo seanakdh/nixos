b5a27cb7881eddc47c8d43deaeef58352231dc8a
seanakdh <sean@technical.ohanlon.net>
2024-04-30 19:44:51 +0200

    added zig for dev and automount for backup drive

--- system/applications/development.nix
+++ system/applications/development.nix
@@ -8,18 +8,24 @@
     cmake
     unstable.kotlin-language-server
     kotlin
+    ktlint
     unstable.rust-analyzer
-    unstable.rustc
+    # unstable.rustc
     unstable.cargo
-    unstable.rustfmt
+    # unstable.rustfmt
     jetbrains.idea-community
     nil
+    nixfmt
     unstable.vscodium
     unstable.neovim
     jdk21
     graphviz
     # unstable.nixd
     # unstable.rnix-lsp
+    shfmt
+    shellcheck
+    zig
+    zls
   ];
   programs.java.enable = true;
 }
--- system/applications/utils.nix
+++ system/applications/utils.nix
@@ -1,4 +1,14 @@
-{ config, lib, pkgs, unstable, ... }:
+{ config, lib, pkgs, unstable, ... }:let
+
+  backup-script = pkgs.writeScriptBin "backup" ''
+    #! ${pkgs.runtimeShell}
+
+    # sleep 10
+
+    # ${pkgs.rsync.outPath}/bin/rsync -ar /home/sean/ /mnt/backup/sean
+
+  '';
+in 
 
 {
   environment.systemPackages = with pkgs; [
@@ -20,7 +30,22 @@
     sqlite
     p7zip
     xarchiver
+    ansible
   ];
+  systemd.mounts = [{
+    what = "/dev/backup_drive";
+    where = "/mnt/backup";
+    type = "ext4";
+  }
+  ];
+  systemd.automounts = [{
+    where = "/mnt/backup";
+    wantedBy = [ "multi-user.target" ];
+  }
+  ];
+  services.udev.extraRules = ''
+    KERNEL=="sd?1", ATTRS{serial}=="S6XJNS0W739253A", SYMLINK+="backup_drive", NAME="Backup Drive", OWNER="sean", GROUP="users"
+  '';
   services.locate.localuser = null;
   services.locate.package = pkgs.mlocate;
   services.locate.enable = true;
