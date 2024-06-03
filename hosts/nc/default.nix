d4b83958c70c9f2d68ab915f98fc518079e8391f
seanakdh <sean@technical.ohanlon.net>
2024-06-03 19:31:50 +0200

    forgot sshd.nix

--- hosts/nc/default.nix
+++ hosts/nc/default.nix
@@ -12,6 +12,7 @@
     ./hardware-configuration.nix
     ../../system/applications/server_base.nix
     ../../system/services/nextcloud.nix
+    ../../system/services/sshd.nix
     ../../system/base/sh.nix
     ../../system/base/boot.nix
     ../../system/env
